153881a3cfec687c526f7446cd5e74b2f93ede6a
shinobiseb <sebpatin@gmail.com>
2024-05-24 16:03:30 -0400

    started mobile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,25 +5,16 @@ interface ItemRefsType {
 }
 
 const sidebarArr = [
-  {
-    name: 'PROJECTS',
-    key: 0
-  },
-  {
-    name: 'ABOUT',
-    key: 1
-  },
-  {
-    name: 'CONTACT',
-    key: 2
-  }
+  { name: 'PROJECTS', key: 0 },
+  { name: 'ABOUT', key: 1 },
+  { name: 'CONTACT', key: 2 }
 ];
 
 export default function Sidebar() {
   const itemRefs = useRef<ItemRefsType>({});
 
   function lengthFunction (key : any) {
-    const result = (1/(key.innerHTML.length) * 2)
+    const result = ((key.innerHTML.length) )
     return result
   }
 
@@ -31,7 +22,7 @@ export default function Sidebar() {
     const item = itemRefs.current[key];
     if (item) {
       item.style.color = '#fffcf2';
-      item.style.translate = `-${lengthFunction(item)}em`;
+      item.style.translate = `-${lengthFunction(item) * 3}px`;
     }
   };
 
@@ -39,18 +30,24 @@ export default function Sidebar() {
 
     const item = itemRefs.current[key];
     if (item) {
-      item.style.color = '#252422';
-      item.style.translate = `${lengthFunction(item)}em`;
+      item.style.color = '#232422';
+      item.style.translate = `${lengthFunction(item) * 3}px`;
     }
   };
 
   return (
-    <div className='z-20 h-full sm:w-1/2 bg-orange' id='Sidebar'>
-      <ul id='Sidebar-list' className='flex flex-col justify-evenly h-full text-right sm:text-7xl ml-4'>
+    <div className='z-20 h-full w-2/3 sm:w-1/2 bg-orange' id='Sidebar'>
+      <ul id='Sidebar-list' className='flex flex-col justify-between h-full text-right ml-4 py-4'>
+
+        <div className='sidebar-header text-black flex flex-col justify-between h-20 mt-4 pr-5'>
+          <h3 className='sidebar-header text-xl md:text-4xl'>Ney Sebastian Patin III</h3>
+          <h4 className='sidebar-header text-xl md:text-3xl'>Software Engineer</h4>
+        </div>
+        
         {sidebarArr.map((item) => (
           <li 
             key={item.key} 
-            className='sidebar-item text-black sidebar-item cursor-pointer' 
+            className='sidebar-item text-black sidebar-item cursor-pointer text-2xl md:text-9xl' 
             onMouseOver={() => handleMouseOver(item.key)} 
             onMouseOut={() => handleMouseOut(item.key)}
             ref={(element) => (itemRefs.current[item.key] = element)}
@@ -58,6 +55,7 @@ export default function Sidebar() {
             {item.name}
           </li>
         ))}
+
       </ul>
     </div>
   );
