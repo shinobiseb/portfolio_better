23f893eb63a48e5bc14636ba409707ee2792b1ac
shinobiseb <sebpatin@gmail.com>
2024-05-28 19:47:00 -0400

    reworked sidebar - less fluff better appearance and consistency

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,16 +13,11 @@ const sidebarArr = [
 export default function Sidebar() {
   const itemRefs = useRef<ItemRefsType>({});
 
-  function lengthFunction(element: HTMLAnchorElement) {
-    const result = element.innerHTML.length;
-    return result;
-  }
-
   const handleMouseOver = (key: number) => {
     const item = itemRefs.current[key];
     if (item) {
       item.style.color = '#fffcf2';
-      item.style.transform = `translateX(-${lengthFunction(item) * 3}px)`;
+      item.style.transform = `translateX(-30px)`;
     }
   };
 
@@ -30,13 +25,13 @@ export default function Sidebar() {
     const item = itemRefs.current[key];
     if (item) {
       item.style.color = '#232422';
-      item.style.transform = `translateX(${lengthFunction(item) * 3}px)`;
+      item.style.transform = `translateX(0px)`;
     }
   };
 
   return (
     <div className='z-20 h-full w-2/3 sm:w-1/2 bg-orange' id='Sidebar'>
-      <ul id='Sidebar-list' className='flex flex-col justify-between h-full text-right ml-4 py-4'>
+      <ul id='Sidebar-list' className='flex flex-col justify-around h-full text-right ml-4 py-4'>
         <div className='sidebar-header text-black flex flex-col justify-between h-20 mt-4 pr-5'>
           <h3 className='sidebar-header text-xl md:text-4xl'>Ney Sebastian Patin III</h3>
           <h4 className='sidebar-header text-xl md:text-3xl'>Software Engineer</h4>
@@ -45,7 +40,7 @@ export default function Sidebar() {
         {sidebarArr.map((item) => (
           <a 
             key={item.key} 
-            className='sidebar-item text-black cursor-pointer text-2xl sm:text-7xl lg:text-9xl' 
+            className='sidebar-item text-black cursor-pointer text-2xl sm:text-7xl lg:text-9xl pr-5' 
             onMouseOver={() => handleMouseOver(item.key)} 
             onMouseOut={() => handleMouseOut(item.key)}
             ref={(element) => (itemRefs.current[item.key] = element)}
