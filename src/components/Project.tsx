7a846f8ae05a9cd979dbfc75da0d4fff53593607
shinobiseb <sebpatin@gmail.com>
2024-05-30 13:55:00 -0400

    small changed to project list mvp

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,12 +7,12 @@ interface projectProps {
 
 export default function Project( props : projectProps ) {
   return (
-    <div className='project-div flex flex-row justify-between items-center w-full bg-orange h-24 px-8 mb-2'>
+    <div className='project-div flex flex-row justify-between items-center w-full bg-orange h-24 px-8 mb-2 cursor-pointer overflow-hidden'>
         <h3 className='text-3xl font-bold truncate uppercase text-black'>{props.title}</h3>
         <ul className='flex flex-row w-7/12 justify-between text-xl'>
-            <li className='project-tag uppercase'>{props.tags[0]}</li>
-            <li className='project-tag uppercase'>{props.tags[1]}</li>
-            <li className='project-tag uppercase'>{props.tags[2]}</li>
+            <li className='project-tag uppercase italic'>{props.tags[0]}</li>
+            <li className='project-tag uppercase italic'>{props.tags[1]}</li>
+            <li className='project-tag uppercase italic'>{props.tags[2]}</li>
         </ul>
     </div>
   )
