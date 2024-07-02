140da5e636670a8f55e4a072371ff07bbebc6c6b
shinobiseb <sebpatin@gmail.com>
2024-07-02 12:56:47 -0400

    adjusted colors to be more contrasted

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Home from "./components/Home";
 // Define the type for project information
 function App() {
   return (
-    <main id='App' className="h-screen bg-gray flex flex-row justify-center items-center">
+    <main id='App' className="h-screen bg-black flex flex-row justify-center items-center">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<ProjectList/>} />
