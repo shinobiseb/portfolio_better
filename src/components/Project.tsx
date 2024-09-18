ddc3d199f87f3cd9ac1dd49655533edb1a95db5c
shinobiseb <sebpatin@gmail.com>
2024-09-18 00:34:51 -0400

    project tag appears at the end

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,12 +4,12 @@ export default function Project( props : projectProps ) {
 
   return (
     <>
-        <div className='project-div  flex flex-row justify-between items-center w-full bg-orange h-24 px-8 mb-2 cursor-pointer overflow-hidden'>
+        <div className='project-div  flex flex-row justify-between items-center w-full bg-orange h-24 px-8 mb-2 cursor-pointer'>
             <h3 className='text-xl sm:text-2xl font-bold truncate uppercase text-black'>{props.title}</h3>
-            <ul className='sm:flex flex-row w-7/12 sm:w-1/2 justify-between text-sm hidden'>
-                <li className='project-tag capitalize italic justify-center truncate items-center flex text-white font-semibold'>{props.tags[0]}</li>
-                <li className='project-tag capitalize italic justify-center truncate items-center flex text-white font-semibold'>{props.tags[1]}</li>
-                <li className='project-tag capitalize italic justify-center truncate items-center flex text-white font-semibold'>{props.tags[2]}</li>
+            <ul className='sm:flex flex-row w-1/6 sm:w-1/2 justify-between text-sm'>
+                <li className='project-tag capitalize italic justify-center truncate items-center text-white font-semibold'>{props.tags[0]}</li>
+                <li className='project-tag capitalize italic justify-center truncate items-center text-white font-semibold hidden sm:flex'>{props.tags[1]}</li>
+                <li className='project-tag capitalize italic justify-center truncate items-center text-white font-semibold hidden sm:flex'>{props.tags[2]}</li>
             </ul>
         </div>
     </>
