6bdd5d0f505319fdf8cb7f2473eaef39a1e5e32c
shinobiseb <sebpatin@gmail.com>
2024-09-20 13:51:06 -0400

    removed a variable that wasn't being used

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ProjectList from './components/ProjectList';
 import About from './components/About';
 import Home from './components/Home';
